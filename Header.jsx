b6d72d93e3b36e5d6ac6b6f9ca0b1ba8b1c503e9
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2024-11-21 15:37:36 -0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom"
+
+function Header (){
+    return(
+        <>
+        <header class="container-fluid text-primary text-center p-3">
+            <h1>Energia Limpa e um Futuro Sustentável</h1>
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><NavLink class="nav-link maldito" to="/">Home</NavLink></li>
+                        <li class="nav-item"><NavLink class="nav-link maldito" to="/info">Informações</NavLink></li>
+                        <li class="nav-item"><NavLink class="nav-link maldito" to="/beneficio">Benefícios</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header
