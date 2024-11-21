c4fdcf0995fcd20155bddf7dde827b482564b29b
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2024-11-21 15:42:21 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 function Header (){
     return(
         <>
-        <header class="container-fluid bg-primary text-white text-center p-3">
+        <header class="container-fluid text-primary text-center p-3">
             <h1>Energia Limpa e um Futuro Sustentável</h1>
             <nav class="navbar navbar-expand-lg navbar-dark">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,9 +11,9 @@ function Header (){
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><NavLink class="nav-link" to="/">Home</NavLink></li>
-                        <li class="nav-item"><NavLink class="nav-link" to="/info">Informações</NavLink></li>
-                        <li class="nav-item"><NavLink class="nav-link" to="/beneficio">Benefícios</NavLink></li>
+                        <li class="nav-item"><NavLink class="nav-link maldito" to="/">Home</NavLink></li>
+                        <li class="nav-item"><NavLink class="nav-link maldito" to="/info">Informações</NavLink></li>
+                        <li class="nav-item"><NavLink class="nav-link maldito" to="/beneficio">Benefícios</NavLink></li>
                     </ul>
                 </div>
             </nav>
