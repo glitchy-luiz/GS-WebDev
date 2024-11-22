9aed163041eecdc5ff71de0ffd020b86b3030011
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2024-11-22 10:42:43 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import { NavLink } from "react-router-dom"
 function Header (){
     return(
         <>
-        <header class="container-fluid text-primary text-center p-3">
+        <header className="container-fluid bg-primary text-white text-center p-3">
             <h1>Energia Limpa e um Futuro Sustentável</h1>
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><NavLink class="nav-link maldito" to="/">Home</NavLink></li>
-                        <li class="nav-item"><NavLink class="nav-link maldito" to="/info">Informações</NavLink></li>
-                        <li class="nav-item"><NavLink class="nav-link maldito" to="/beneficio">Benefícios</NavLink></li>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item"><NavLink className="nav-link" to="/">Home</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="/info">Informações</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="/beneficio">Benefícios</NavLink></li>
                     </ul>
                 </div>
             </nav>
